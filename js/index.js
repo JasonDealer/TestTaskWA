96ac917a5f39c406d1f84ba193d547dd56a8238c
Maksim Kovler <makcxx.work@gmail.com>
2021-08-17 23:02:34 +0300

    added getters/setters + readonly mode

--- js/index.js
+++ js/index.js
@@ -1,6 +1,79 @@
 const text = document.querySelector(".form__field"),
-    addButton = document.querySelector(".addButton"),
-    tagArea = document.querySelector(".tagArea");
+      addButton = document.querySelector(".addButton"),
+      tagArea = document.querySelector(".tagArea"),
+      modeSwitcher = document.querySelector("#cbx");
+
+let tagNames = [];
+
+function refreshTagList() {
+    let tagList = document.querySelectorAll(".tagName");
+    tagList.forEach(item => {
+        tagNames.push(item.textContent);
+    });
+}
+
+//resresh
+
+refreshTagList();
+
+//getters/setters
+    
+let tags = {
+    tagNames: [],
+    get tagList() {
+        return this.tagNames.join();
+    },
+    set tagList(newTagList) { //Enter new tag list (Example: ["blue", "green", "yellow"])
+        this.tagNames = newTagList;
+        tagArea.innerHTML = ``;
+        this.tagNames.forEach(element => {
+            tagArea.innerHTML += `
+                <div class="tag">
+                    <div class="tagName">${element}</div>
+                    <div class="closeBtn">
+                        <span class="close"></span>
+                    </div>
+                </div>
+            `;
+        });
+        return this.tagNames = newTagList;
+    },
+    set addOneTag(item) {
+        tagArea.innerHTML += `
+            <div class="tag">
+                <div class="tagName">${item}</div>
+                <div class="closeBtn">
+                    <span class="close"></span>
+                </div>
+            </div>
+        `;
+        return this.tagNames.push(item);
+    },
+    set deleteOneTag(element) {
+        this.tagNames = this.tagNames.filter(i => i !== element);
+        tagArea.innerHTML = ``;
+        this.tagNames.forEach(element => {
+            tagArea.innerHTML += `
+                <div class="tag">
+                    <div class="tagName">${element}</div>
+                    <div class="closeBtn">
+                        <span class="close"></span>
+                    </div>
+                </div>
+            `;
+        });
+        return this.tagNames;
+    },
+    set readonlyMode(condition) {
+        if (condition === true) {
+            toggleMode(condition);
+            modeSwitcher.checked = true;
+        } else if (condition === false) {
+            toggleMode(condition);
+            modeSwitcher.checked = false;
+        }
+    }
+}    
 
 //get tag name from input
 
@@ -23,7 +96,8 @@ function createTag() {
                 </div>
             </div>
         `;
-        text.value = " ";
+        text.value = "";
+        refreshTagList();
     }
 }
 
@@ -36,7 +110,28 @@ function deleteTag(e) {
             tagArea.removeChild(entry.parentElement);
         }
     });
+    refreshTagList();
 }
+
+//readonly mode
+
+function toggleMode(condition) {
+    if (modeSwitcher.checked || condition === true) {
+        text.disabled = true;
+        addButton.disabled = true;
+        addButton.classList.add("buttonDisabled");
+        document.querySelectorAll(".closeBtn").forEach(element => {
+            element.classList.add("disableClose");
+        });
+    } else if(modeSwitcher.checked != true || condition === false) {
+        text.disabled = false;
+        addButton.disabled = false;
+        addButton.classList.remove("buttonDisabled");
+        document.querySelectorAll(".closeBtn").forEach(element => {
+            element.classList.remove("disableClose");
+        });
+    }
+};
 
 //btns
 
@@ -46,4 +141,8 @@ addButton.addEventListener("click", ()=> {
 
 tagArea.addEventListener("click", (e)=> {
     deleteTag(e);
+});
+
+modeSwitcher.addEventListener("click", ()=> {
+    toggleMode()
 });
